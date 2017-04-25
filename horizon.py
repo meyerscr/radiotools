e1d172200cd3b9fa335b4cfa3627f14eaf03bf4e
charliemeyers@gmail.com <rojavatechops@gmail.com>
2017-04-25 20:51:10 +0300

    updated horizon script to accept argument from the command line

--- horizon.py
+++ horizon.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 #This script assumes the earth is a perfect sphere, resulting in calculations within 1% of the actual result
+import sys
 earthRadius = 6371000 #in meters
-height = input ("Enter the height (in meters):")
+#height = input ("Enter the height (in meters):")
+height = int(sys.argv[1])
 distance = (2*earthRadius*height+height*height)**.5
-print 'The distance to the horizon is approximately:', int(distance), 'meters'
+print int(distance)
